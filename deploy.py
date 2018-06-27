eb3aaa1cdb20fb9a379eaa482c612db94292a71c
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2018-06-27 11:41:53 -0400

    FLask app

--- deploy.py
+++ deploy.py
@@ -1,11 +1,38 @@
+from flask import Flask, request, flash, jsonify, render_template, make_response
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
-from flask import Flask
-
+DEBUG = True
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
-@app.route('/')
+
+class ReusableForm(Form):
+	words = TextField('Words:', validators=[validators.required()])
+
+
+@app.route("/")
 def index():
-	return "Testing changes"
+	form = ReusableForm(request.form)
+	print(form.errors)
+	if request.method == 'POST':
+		words = request.form['words']
+		print(words)
+
+		if not form.validate():
+			flash('All the form fields are required. ')
+
+	return render_template('index.html', form=form)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def predict():
+	# # Take data value and get features
+	words = request.form['words']
+	print(words)
+
+	return "This works"
+
 
 if __name__ == "__main__":
 	app.run()
